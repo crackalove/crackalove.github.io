fe42ce8d914a6ae24ba365cf9727f450855ced33
crackalove <crackalove@users.noreply.github.com>
2026-01-07 12:12:31 +0300

    fix media structure

--- src/app/consts/media.js
+++ src/app/consts/media.js
@@ -1,6 +1,10 @@
 import proxy from "../proxies/media"
 
 const media = {
+    discord: {
+    id: "1126114403788541983",
+    tag: "scamnut",
+    },
     telegram: "UnRezolved",
     github: "crackalove",
     email: "crackacodes@gmail.com"
