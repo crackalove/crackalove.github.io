d6a8f8b3efa6cbc2a2cb043574780166bf139fba
crackalove <crackalove@users.noreply.github.com>
2026-01-07 12:00:01 +0300

    deploy static build to root

--- src/app/consts/skills.js
+++ src/app/consts/skills.js
@@ -1,7 +1,15 @@
 export default {
-    language: ["js", "ts", "python"],
-    database: ["sqlite", "postgreSql"],
-    other: ["html", "css", "sass", "scss", "less", "stylus", "ejs", "jinja", "pug"],
-    tool: ["vscode", "nvim", "figma", "git"],
-    framework: ["react", "next", "gulp", "express", "flask", "quart"]   
+    language: ["js", "cpp", "python"],
+    database: ["sqlite"],
+    other: ["html", "css", "sass", "assembly", "elf", "pe"],
+    tool: [
+        "ida",
+        "x64dbg",
+        "httpDebugger",
+        "vscode",
+        "procmon",
+        "burp",
+        "git"
+    ],
+    framework: ["flask"]
 }
